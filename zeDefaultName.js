b44e843a6c356b8e8ad4b2538371fca25096152a
hstgkr <73450831+hstgkr@users.noreply.github.com>
2020-11-20 02:54:39 +0700

    HMTL Basics

---
+++ zeDefaultName.js
@@ -0,0 +1,47 @@
+function test() {
+    alert("Thank you for contacting us! We'll reach back to you soon!")
+}
+
+/* ----- */
+
+function gobuy1() {
+    alert("Bought the Standard Pack!")
+}
+
+function gobuy2() {
+    alert("Bought the Ultimate Pack!")
+}
+
+/* ----- */
+
+function wolf() {
+    alert(`Bought the Wolf Pet!`)
+}
+
+function bear() {
+    alert(`Bought the Bear Pet!`)
+}
+
+function wisp() {
+    alert(`Bought the Wisp Pet!`)
+}
+
+/*
+function wolf() {
+    alert(`Bought the Wolf Pet for $${wolfPrice}!`)
+}
+
+function bear() {
+    alert(`Bought the Bear Pet for $${bearPrice}!`)
+}
+
+function wisp() {
+    alert(`Bought the Wisp Pet for $${wispPrice}!`)
+}
+*/
+
+/* ----- */
+
+function finalPurchase() {
+    alert("Thank you for buying!")
+}
